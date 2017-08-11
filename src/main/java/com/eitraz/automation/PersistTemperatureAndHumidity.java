dad8dca6cb4cddc283904b425eed66c92b65061e
Eitraz <git@eitraz.com>
2017-08-11 21:34:24 +0200

    Set timestamp from localdatetime again

--- src/main/java/com/eitraz/automation/PersistTemperatureAndHumidity.java
+++ src/main/java/com/eitraz/automation/PersistTemperatureAndHumidity.java
@@ -31,7 +31,7 @@ public class PersistTemperatureAndHumidity {
     public void handleSensorUpdate(AbstractTemperatureAndHumiditySensor sensor) {
         TemperatureHumidityLogEntity entity = new TemperatureHumidityLogEntity();
         entity.setSensor(sensor.getClass().getSimpleName());
-        entity.setDatetime(new Timestamp(sensor.getLastEventTime().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()));
+        entity.setDatetime(Timestamp.valueOf(sensor.getLastEventTime()));
         entity.setTemperature(new BigDecimal(sensor.getTemperature()));
         entity.setHumidity(new BigDecimal(sensor.getHumidity()));
 
