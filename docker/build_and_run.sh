5338efbe16f422cb0ac15f827c8c449b22ba1fb1
Eitraz <git@eitraz.com>
2017-08-11 19:02:35 +0200

    Updated build script

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -4,4 +4,6 @@ git pull && \
 mvn clean package -DskipTests && \
 cd target && \
 cp home-automation-*.jar home-automation.jar && \
+mv home-automation.jar / && \
+cd / && \
 java -Dip=$1 -jar home-automation.jar
